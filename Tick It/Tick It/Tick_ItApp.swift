9d80a9472bd95b9d33a3e03843ce151bfa6b8dbd
Karsten Westra <karsten@gpal.nl>
2023-05-24 09:48:11 +0200

    Added basic Xcode project with app for Mac/iOS using SwiftUI.

---
+++ Tick It/Tick It/Tick_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tick_ItApp.swift
+//  Tick It
+//
+//  Created by Karsten Westra on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tick_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tick It/Tick It/Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tick It
+//
+//  Created by Karsten Westra on 24/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "calendar.circle")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, Tick 't!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
